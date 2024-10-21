a0352bf9253d3eb3b2dc2b297de7560a59856e22
Samuel Thomaz Bastos <samuel-tb@hotmail.com>
2024-10-21 14:54:25 -0300

    created preprocessing module and fixed utils_file

--- utils_file.py
+++ utils_file.py
@@ -6,6 +6,12 @@ import pandas as pd
 import torch 
 import random
 
+from tqdm import tqdm
+
+from input.reading_datasets import read_dataset_from_file
+from preprocessing.get_dummies_labels import GetDummiesLabels
+from preprocessing.train_test_split_module import TrainTestSplit
+
 def set_seeds(seed):
     os.environ['PYTHONHASHSEED'] = str(seed)
     random.seed(seed)
@@ -42,3 +48,42 @@ def get_all_results():
     complete_data = pd.DataFrame(info)
         
     return complete_data
+
+
+def training_nn_for_seeds(used_model, datasets = [], seeds = []):
+    for dataset in tqdm(datasets):
+        for random_state in tqdm(seeds):
+            print(f'{dataset} - {random_state}')
+            used_dataset = read_dataset_from_file(dataset_name = dataset)
+            X, y, metadata = used_dataset
+
+            get_dummies_object = GetDummiesLabels(
+                X_raw= X,
+                y_raw= y,
+                metadata= metadata
+            )
+
+            X, y = get_dummies_object.transform()
+
+            train_test_object = TrainTestSplit(
+                X_raw= X,
+                y_raw= y,
+                metadata= metadata,
+                random_state = random_state
+            )
+
+            X_train, X_test, y_train, y_test = train_test_object.transform()
+
+            model = used_model(
+                X_train=X_train,
+                X_test = X_test,
+                y_train = y_train,
+                y_test = y_test,
+                metadata = metadata,
+                random_state = random_state
+            )
+
+            if len(os.listdir('./model_checkpoints/' + model.model_folder)) != 0 :
+                pass
+            else:
+                model.training_process()
