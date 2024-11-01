a5074856c60975f757d052306f1ebe599af02913
Samuel Thomaz Bastos <samuel-tb@hotmail.com>
2024-10-31 22:54:55 -0300

    Added eigenworms

--- main_fcn_gap.py
+++ main_fcn_gap.py
@@ -5,7 +5,7 @@ from utils_file import training_nn_for_seeds
 
 
 training_nn_for_seeds(
-    datasets= ['ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'RacketSports', 'ArticularyWordRecognition', 'AtrialFibrillation', 'StandWalkJump'], 
+    datasets= ['ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'RacketSports', 'ArticularyWordRecognition', 'AtrialFibrillation', 'StandWalkJump', 'EigenWorms'], 
     seeds= list(range(1, 11)),
     used_model = FullyConvolutionalNetwork1DGAP
 )
