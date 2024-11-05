77125447836344d9a10a88e661f9d61ace14f1fd
Samuel Thomaz Bastos <samuel-tb@hotmail.com>
2024-11-05 15:06:20 -0300

    created inception time with late fusion

---
+++ main_inception_time_ensemble_late.py
@@ -0,0 +1,12 @@
+from models.inception_time_ensemble_late import InceptionTimeEnsembleLate
+from utils_file import training_nn_for_seeds
+
+
+
+training_nn_for_seeds(
+    # datasets= ['ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'RacketSports', 'ArticularyWordRecognition', 'AtrialFibrillation', 'StandWalkJump', 'EigenWorms'], 
+    datasets= ['ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'RacketSports', 'ArticularyWordRecognition', 'AtrialFibrillation', 'StandWalkJump'], 
+    seeds= list(range(1, 11)),
+    used_model = InceptionTimeEnsembleLate,
+    is_debbug=False
+)   
