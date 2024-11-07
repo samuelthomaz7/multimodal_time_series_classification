5a29dbc4dd1cd7750deb19d11bd355706a9629e0
Samuel Thomaz Bastos <samuel-tb@hotmail.com>
2024-11-07 10:42:38 -0300

    added error bars to the notebook plot

--- main_inception_time_ensemble_late.py
+++ main_inception_time_ensemble_late.py
@@ -5,7 +5,8 @@ from utils_file import training_nn_for_seeds
 
 training_nn_for_seeds(
     # datasets= ['ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'RacketSports', 'ArticularyWordRecognition', 'AtrialFibrillation', 'StandWalkJump', 'EigenWorms'], 
-    datasets= ['ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'RacketSports', 'ArticularyWordRecognition', 'AtrialFibrillation', 'StandWalkJump'], 
+    # datasets= ['ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'RacketSports', 'ArticularyWordRecognition', 'AtrialFibrillation', 'StandWalkJump'], 
+    datasets= ['AtrialFibrillation', 'StandWalkJump', 'RacketSports', 'ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'ArticularyWordRecognition'], 
     seeds= list(range(1, 11)),
     used_model = InceptionTimeEnsembleLate,
     is_debbug=False
