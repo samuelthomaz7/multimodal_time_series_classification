47a93fafaaaadef9a6cedee04ce121611edd8021
Samuel Thomaz Bastos <samuel-tb@hotmail.com>
2024-12-02 09:14:37 -0300

    included new metrics on test_notebook

--- main_lite_time_late.py
+++ main_lite_time_late.py
@@ -4,8 +4,8 @@ from utils_file import training_nn_for_seeds
 
 
 training_nn_for_seeds(
-    datasets= ['ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'RacketSports', 'ArticularyWordRecognition', 'AtrialFibrillation', 'StandWalkJump'], 
-    # datasets=['AtrialFibrillation', 'StandWalkJump', 'RacketSports', 'ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'ArticularyWordRecognition'],
+    # datasets= ['ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'RacketSports', 'ArticularyWordRecognition', 'AtrialFibrillation', 'StandWalkJump'], 
+    datasets=['AtrialFibrillation', 'StandWalkJump', 'RacketSports', 'ArticularyWordRecognition', 'BasicMotions', 'Cricket', 'NATOPS', 'ArticularyWordRecognition'],
     seeds= list(range(1, 11)),
     used_model = LITELate,
     is_debbug=False,
